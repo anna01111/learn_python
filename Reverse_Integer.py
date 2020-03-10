0011402110b63d588a48b50166f0b654f4aa55ac
Anna <anna.lozynska@globallogic.com>
2020-03-10 11:30:46 +0200

    making changes

---
+++ Reverse_Integer.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+# Status - In Progress
+
+"""
+Given a 32-bit signed integer, reverse digits of an integer.
+
+Example 1:    Input: 123    Output: 321
+
+Example 2:    Input: -123   Output: -321
+
+Example 3:    Input: 120    Output: 21
+
+Note:
+Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range:
+[−2**31, 2**31, − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer
+overflows.
+"""
+
+my_num = int(input("Please enter a whole number from the range between - 2147483648, 2147483648 : "))
+print(type(my_num))
+
+
+def reverse_digits(user_num):
+    pass
