b0745b945f39cd03c820bea543cbb1dbfc0140aa
Anna <anna.lozynska@globallogic.com>
2020-03-24 10:10:46 +0200

    -

--- Sea_Battle.py
+++ Sea_Battle.py
@@ -1,9 +1,27 @@
 
-def create_board():
-    pass
+empty = ' '
+ship_alive = '0'
+ship_killed = 'x'
+missed = '-'
 
 
-def print_board():
-    pass
+def create_board(size):
+    board = []
+    for i in range(size):
+        board.append(['o' for el in range(size)])
+    return board
 
 
+def print_board(board):
+    board_to_print = board
+    letters = ['  ', 'A ', 'B ', 'C ', 'D ', 'E ', 'F ', 'G ', 'H ', 'I ', 'J ']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    for i in range(len(board_to_print)):
+        board_to_print[i].insert(0, numbers[i])
+        board_to_print[i].append('')
+    print(''.join(map(''.join, letters)))
+    print('\n'.join(map('|'.join, board_to_print)))
+
+
+my_board = create_board(10)
+print_board(my_board)
