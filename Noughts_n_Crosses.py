1a58fd50ed5342eeb6216eb20ee3b52998595f90
Anna <anna.lozynska@globallogic.com>
2020-03-27 20:11:27 +0200

    -

--- Noughts_n_Crosses.py
+++ Noughts_n_Crosses.py
@@ -1,5 +1,13 @@
 
 def create_board():
+    """Creates a 3x3 board
+
+    Returns
+    -------
+    list
+        A list containing three lists, each of which contains three strings
+    """
+
     board = [
         [' ', ' ', ' '],
         [' ', ' ', ' '],
@@ -9,6 +17,14 @@ def create_board():
 
 
 def print_board(board):
+    """Prints a 3x3 board
+
+    Parameters
+    ----------
+    board : list
+        A list containing three lists, each of which contains three strings
+    """
+
     print(' {} | {} | {} '.format(board[0][0], board[0][1], board[0][2]))
     print('___|___|___')
     print(' {} | {} | {} '.format(board[1][0], board[1][1], board[1][2]))
@@ -18,35 +34,54 @@ def print_board(board):
 
 
 def place_sign(board, signs):
+    """Places the sign on the board
+
+    Parameters
+    ----------
+    board : list
+        A list containing three lists, each of which contains three strings
+    signs : list
+        A list containing two strings - each string represents a sign
+    """
+
     try:
         print_board(board)
         row = int(input('Enter a row to place ' + signs[0] + ' (1, 2 or 3): '))
         col = int(input('Enter a col to place ' + signs[0] + ' (1, 2 or 3): '))
         if board[row - 1][col - 1] == ' ':
-            board[row - 1][col - 1] = signs[0]
+            board[row - 1][col - 1] = signs[0]  # places the sign on the board if the place is free
         else:
-            print('This place is occupied, please try again')
+            print('THIS PLACE IS OCCUPIED, PLEASE TRY AGAIN')
             place_sign(board, signs)
-    except ValueError:
+    except ValueError:  # handles a situation when user input can not be converted to an integer
         print('ERROR - ENTER VALID NUMBER ')
         place_sign(board, signs)
-    except IndexError:
+    except IndexError:  # handles a situation when user input is out of the board's index range
         print('ERROR - ENTER VALID NUMBER ')
         place_sign(board, signs)
 
 
 def winner_check(board):
+    """Checks if there is a winner
+
+    Parameters
+    ----------
+    board : list
+        A list containing three lists, each of which contains three strings
+
+    Returns
+    -------
+    bool or str
+        False if there is no winner or string representing a winner if there is a winner
     """
-    :param board:
-    :return: False if there is no winner, winner if there is a winner
-    """
+
     winner = False
-    for i in range(3):
-        if board[i][0] == board[i][1] == board[i][2] != ' ':
+    for i in range(len(board)):
+        if board[i][0] == board[i][1] == board[i][2] != ' ':  # looks for a winner in the horizontal lines
             winner = board[i][0]
-        elif board[0][i] == board[1][i] == board[2][i] != ' ':
+        elif board[0][i] == board[1][i] == board[2][i] != ' ':  # looks for a winner in the vertical lines
             winner = board[0][i]
-    if board[0][0] == board[1][1] == board[2][2] != ' ':
+    if board[0][0] == board[1][1] == board[2][2] != ' ':  # looks for a winner in the diagonals
             winner = board[0][0]
     elif board[0][2] == board[1][1] == board[2][0] != ' ':
             winner = board[0][2]
@@ -54,19 +89,26 @@ def winner_check(board):
 
 
 def draw_check(board):
+    """Checks if there is a draw
+
+    Parameters
+    ----------
+    board : list
+        A list containing three lists, each of which contains three strings
+
+    Returns
+    -------
+    bool
+        True if there is a draw, False if there is no draw
     """
-    :param board:
-    :return: True if there is a draw ans False if not
-    """
+
     for el in board:
         if ' ' in el:
             return False
-        else:
-            continue
     return True
 
 
-my_signs = ['x', '0']
+my_signs = ['x', 'o']
 my_board = create_board()
 print('Welcome to my game ^ ^')
 while True:
@@ -79,8 +121,7 @@ while True:
         print('There is a draw')
         break
     place_sign(my_board, my_signs)
-    my_signs = my_signs[::-1]
-    continue
+    my_signs = my_signs[::-1]  # swaps the signs so that players can take turns
 
 
 
--- Sea_Battle.py
+++ Sea_Battle.py
@@ -1,27 +1,62 @@
+import string
+import random
 
+# defining a cell status
 empty = ' '
-ship_alive = '0'
+ship_alive = '■'
 ship_killed = 'x'
-missed = '-'
+missed = '·'
 
 
 def create_board(size):
     board = []
     for i in range(size):
-        board.append(['o' for el in range(size)])
+        board.append([' ' for el in range(size)])
     return board
 
 
 def print_board(board):
     board_to_print = board
-    letters = ['  ', 'A ', 'B ', 'C ', 'D ', 'E ', 'F ', 'G ', 'H ', 'I ', 'J ']
-    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    letters = list(string.ascii_uppercase)
+    letters.insert(0, ' ')
+    letters = letters[:len(board) + 1]
+    letters.append('')
+    numbers = [num for num in range(len(board) + 1)]
+    numbers = list(map(str, numbers))
     for i in range(len(board_to_print)):
         board_to_print[i].insert(0, numbers[i])
         board_to_print[i].append('')
-    print(''.join(map(''.join, letters)))
-    print('\n'.join(map('|'.join, board_to_print)))
+    board_to_print.insert(0, letters)
+    for i in range(len(board_to_print)):
+        print(' | '.join(board_to_print[i]))
+        print('__|_' * (len(board_to_print) - 1) + '__|')
+
+
+def put_ships(board):  # one of core functions
+    pass
+
+
+def put_a_ship(board, ship_size):
+    pass
+
+
+def check_if_correct_placement(board):
+    pass
+
+
+def reserve_space_around_a_ship(board):
+    pass
+
+
+def fire_a_shot(board):  # one of core functions
+    pass
+
+
+def winner_check(board):  # one of core functions
+    pass
 
 
 my_board = create_board(10)
 print_board(my_board)
+
+
